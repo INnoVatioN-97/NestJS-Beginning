d217862d7e071311db274d05dc993c4ee42281bb
INnoVatioN-97 <koyoungil97@gmail.com>
2022-07-17 19:04:28 +0900

    Unit Test 1(getOne, getAll)

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import exp from 'constants';
 import { MoviesService } from './movies.service';
 
 describe('MoviesService', () => {
@@ -14,5 +16,35 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should be return a movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        genres: ['Action', 'gore', 'romance'],
+        year: 2000,
+      });
+      const movie = service.getOne(1);
+
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err.message).toEqual('999에 해당하는 영화를 찾지 못했습니다.');
+      }
+    });
   });
 });
